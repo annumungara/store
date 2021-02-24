322ced72b4b89576e5f2b758fed64ca2b7574a10
annumungara <78908816+annumungara@users.noreply.github.com>
2021-02-24 22:10:56 +0530

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,27 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Orders: undefined;
+  Menu: undefined;
+  Insights: undefined;
+  Store: undefined;
+};
+
+export type OrdersParamList = {
+  OrdersScreen: undefined;
+};
+
+export type MenuParamList = {
+  MenuScreen: undefined;
+};
+
+export type InsightsParamList = {
+  InsightsScreen: undefined;
+};
+
+export type StoreParamList = {
+  StoreScreen: undefined;
+};
